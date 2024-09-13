d9ca8d45f415f255d58a4fbebd48c7b86a923c6c
Daan Mortier <daanmortier98@gmail.com>
2024-09-13 13:56:36 +0200

    add tfe provider

--- main.auto.tfvars
+++ main.auto.tfvars
@@ -1,11 +1,11 @@
-organization_name  = "AE_nv"
+organization_name = "AE_nv"
 projects = [
   {
-    name       = "AE_starters_project_1"
-    workspaces = ["AE_starters_workspace_1", "AE_starters_workspace_2", "AE_starters_workspace_3"]
+    name       = "AE_starters_project_iac"
+    workspaces = ["AE_starters_workspace_steven", "AE_starters_workspace_pieterjan", "AE_starters_workspace_stephanie"]
   },
-#   {
-#     name       = "AE_starters_project_2"
-#     workspaces = ["AE_starters_workspace_1", "AE_starters_workspace_2"]
-#   }
+  #   {
+  #     name       = "AE_starters_project_2"
+  #     workspaces = ["AE_starters_workspace_1", "AE_starters_workspace_2"]
+  #   }
 ]
--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ terraform {
       version = "0.58.1"
     }
   }
-  
+
   cloud {
     organization = "AE_nv"
     workspaces {
-      name = "AE_starters_workspace_remote_backend"
+      name    = "AE_starters_workspace_remote_backend"
       project = "AE_starters_project_tf_backend"
     }
   }
@@ -20,15 +20,42 @@ data "tfe_organization" "org" {
   name = var.organization_name
 }
 
-resource "tfe_project" "projects" {
-  for_each     = { for project in var.projects : project.name => project }
-  name         = each.value.name
-  organization = data.tfe_organization.org.name
+provider "tfe" {
+  token = ""
 }
 
-resource "tfe_workspace" "workspaces" {
-  for_each     = { for workspace in local.flattened_workspaces : "${workspace.project_name}.${workspace.name}" => workspace }
-  name         = each.value.name
+# resource "tfe_project" "projects" {
+#   for_each     = { for project in var.projects : project.name => project }
+#   name         = each.value.name
+#   organization = data.tfe_organization.org.name
+# }
+
+# resource "tfe_workspace" "workspaces" {
+#   for_each     = { for workspace in local.flattened_workspaces : "${workspace.project_name}.${workspace.name}" => workspace }
+#   name         = each.value.name
+#   organization = data.tfe_organization.org.name
+#   project_id   = tfe_project.projects[each.value.project_name].id
+# }
+
+resource "tfe_project" "project" {
+  name         = "AE_starters_project_iac"
+  organization = var.organization_name
+}
+
+resource "tfe_workspace" "workspace_1" {
+  name         = "AE_starters_workspace_steven"
   organization = data.tfe_organization.org.name
-  project_id   = tfe_project.projects[each.value.project_name].id
+  project_id   = tfe_project.project.id
+}
+
+resource "tfe_workspace" "workspace_2" {
+  name         = "AE_starters_workspace_pieterjan"
+  organization = data.tfe_organization.org.name
+  project_id   = tfe_project.project.id
+}
+
+resource "tfe_workspace" "workspace_3" {
+  name         = "AE_starters_workspace_stephanie"
+  organization = data.tfe_organization.org.name
+  project_id   = tfe_project.project.id
 }
